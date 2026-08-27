a6929c2721c7289609bd1e37d704d66584832c89
Max <mugunamaxwel@gmail.com>
2026-08-27 23:34:45 +0300

    Train decision tree and random forest models

--- models/load_datasets.py
+++ models/load_datasets.py
@@ -1,14 +1,14 @@
 from sklearn.model_selection import KFold
 from sklearn.metrics import root_mean_squared_log_error
-import pandas 
+import pandas as pd
 import numpy as np
 
 # Gets the training input and target and test set
 def load_train_test_sets():
-  X_train = pandas.read_csv(r'..\processed_data\X_train.csv')
-  X_test = pandas.read_csv(r'..\processed_data\X_test.csv')
+  X_train = pd.read_csv(r'..\processed_data\X_train.csv')
+  X_test = pd.read_csv(r'..\processed_data\X_test.csv')
 
-  train_target = pandas.read_csv(r'..\processed_data\y_train.csv')
+  train_target = pd.read_csv(r'..\processed_data\y_train.csv')
 
   return X_train, X_test, train_target
 
@@ -30,8 +30,16 @@ def split_train_evaluate(model, train_input, train_target):
     y_train = train_target.iloc[train_index]
     y_val = train_target.iloc[val_index]
 
+    model.fit(X_train, y_train)
+
     train_preds = model.predict(X_train)
     val_preds = model.predict(X_val)
+
+    # For predictions less than 0, return 0 else the value > 0
+    # Carried out since RMSLE does not support -ve values
+    train_preds = np.clip(train_preds, 0, None)
+    val_preds = np.clip(val_preds, 0, None)
+
     train_loss = root_mean_squared_log_error(y_train, train_preds)
     val_loss = root_mean_squared_log_error(y_val, val_preds)
 
@@ -43,4 +51,11 @@ def split_train_evaluate(model, train_input, train_target):
 
   print(f'Training Loss: {avg_train_loss}')
   print(f'Validation Loss: {avg_val_loss}')
-  return model, avg_train_loss, avg_val_loss
+  return model, avg_train_loss, avg_val_loss 
+
+# Creates a csv file to submit to Kaggle
+def create_submission_file(preds, filename):
+  sub_df = pd.read_csv(r'..\sample_submission.csv')
+  sub_df['SalePrice'] = preds
+  sub_df.to_csv(f'..\\submissions\\{filename}.csv', index=None)
+  print(sub_df)
